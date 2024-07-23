71e681cf46082925004646f2c05584d85c7da51a
Filip Kokowski <filipkokowski999@gmail.com>
2024-07-24 00:04:26 +0200

    Added textarea

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import './Home.css'
 
+import { v4 as uuidv4 } from 'uuid';
+
 const InputField = ({type, id, style, placeholder,}) => {
 
   const date = new Date().toJSON().slice(0,10).replace(/-/g,'-');
@@ -10,7 +12,7 @@ const InputField = ({type, id, style, placeholder,}) => {
     
   }, [date])
 
-  return <input defaultValue={(type === 'date' ? date : "")} type={type} id={id} style={style} placeholder={placeholder}></input>
+  return (type == 'textarea') ? <textarea id={id} style={style} placeholder={placeholder}></textarea> : <input defaultValue={(type === 'date' ? date : "")} type={type} id={id} style={style} placeholder={placeholder}></input>;
 }
 
 const Dropdown = ({options, style, id}) => {
@@ -57,7 +59,7 @@ const Form = ({title, setState, style}) => {
         
         <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
           <label>Description</label>
-          <InputField id={'description'} type='text' style={{width: '20vh', height: '5vh', borderRadius: '1.5vh', border: 'none', backgroundColor: '#444444'}}/>
+          <InputField id={'description'} type='textarea' style={{width: '30vh', height: '20vh', borderRadius: '1.5vh', border: 'none', backgroundColor: '#444444'}}/>
         </div>
         
         <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
@@ -81,11 +83,11 @@ const Home = () => {
       <div style={{width: '100vw', height: '75vh', backgroundColor: '#121212'}}>
         <Form title={'Adding form'} setState={setState} style={{width: '100%', height: '100%', display: 'flex', alignItems: 'center'}}></Form>
       </div>
-      <div style={{width: '100vw', height: '75vh', display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
+      <div style={{width: '100vw', height: '25vh', overflowY: 'scroll', display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
         <table style={{fontSize: '1.5vh'}}>
           <tbody>
             <tr><th style={{width: '15vw'}}>Date</th><th style={{width: '15vw'}}>Type</th><th style={{width: '30vw'}}>Contractor</th><th style={{width: '40vw'}}>Description</th></tr>
-            {state.favourites.map((elem) => { return <tr style={{backgroundColor: (new Date().toJSON().slice(0,10).replace(/-/g,'-') == elem.date ? 'red' : '')}} key={elem}><th>{elem.date}</th><th>{elem.type}</th><th>{elem.contractor}</th><th>{elem.description}</th></tr>})}
+            {state.favourites.map((elem) => { return <tr key={uuidv4()} style={{backgroundColor: (new Date().toJSON().slice(0,10).replace(/-/g,'-') == elem.date ? 'red' : '')}}><th>{elem.date}</th><th>{elem.type}</th><th>{elem.contractor}</th><th>{elem.description}</th></tr>})}
           </tbody>
         </table>
       </div>
